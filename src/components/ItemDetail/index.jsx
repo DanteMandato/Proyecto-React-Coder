6dd43663304fb3aabd41e98a9204c04cd411023d
Dante Mandato <dantemandato69@gmail.com>
2024-09-12 19:11:42 -0300

    arreglos y pulida de estilo de detail

--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -7,20 +7,19 @@ const ItemDetail = ({ name, img, price, description, category, dues }) => {
     const onAdd = (qty) => {
         setQuantity(qty);
         console.log(`Agregaste ${qty} unidades al carrito`);
-
     };
 
     return (
         <section className="item-detail">
             <picture className="item-detail__picture">
-                <img className="item__picture item__picture--img" src={`/public/${img}`} alt={name} />
+                <img className="item-detail__img" src={`/public/${img}`} alt={name} />
             </picture>
             <article className="item-detail__info">
-                <p><strong>Categoria:</strong> {category}</p>
-                <h2>{name}</h2>
-                <p><strong>Descripcion:</strong> {description}</p>
-                <p><strong>Precio:</strong> $ {price}</p>
-                <p><strong>Cuotas:</strong> {dues}</p>
+                <h4 className="item-detail__info--category">{category}</h4>
+                <h4 className="item-detail__info--name">{name}</h4>
+                <p className="item-detail__info--price">$ {price} .-</p>
+                <p className="item-detail__info--dues">{dues} CUOTAS SIN INTERÉS</p>
+                <p className="item-detail__info--description">Descripción: {description}</p>
                 <div className="item-detail__count">
                     <ItemCount stock={10} initial={1} onAdd={onAdd} />
                 </div>
